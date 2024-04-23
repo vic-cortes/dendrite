2257751e991adfe8570a5c60b9a4dce68055d3cd
Víctor M. Cortés Figueroa <vmcortesf@gmail.com>
2024-04-23 15:02:49 -0500

    Getting db_variable in setter and getter

--- dendrite/siemens/plc/data_block.py
+++ dendrite/siemens/plc/data_block.py
@@ -18,26 +18,27 @@ class DataBlock:
             raise ValueError("'db_number' must be an integer")
 
         if not (0 <= cls.db_number <= MAX_ALLOWED_DB_NUMBER):
-            raise ValueError("db_number must be between 0 and 65535")
+            raise ValueError("db_number must be between 0 and 20")
 
 
 class BoolField:
 
-    def __init__(self, *, byte_number: int, bit_position: int, db_number: int) -> None:
+    def __init__(self, *, byte_number: int, bit_position: int) -> None:
         self._byte_number = byte_number
         self._bit_position = bit_position
-        self._db_number = db_number
 
         # Keeping track of each instance
         self._values = WeakKeyDictionary()
 
     def __get__(self, instance, instance_type):
         # TODO: Add logic for data retrieve from byte
+        db_number = instance_type.db_number
         if instance is None:
             return self
         return self._values.get(instance)
 
     def __set__(self, instance, value):
+        db_number = instance.db_number
         if not (0 <= value <= MAX_ALLOWED_INTEGER):
             raise ValueError(f"Value must be between 0 and {MAX_ALLOWED_INTEGER}")
         # TODO: Add logic for sending data to PLC
@@ -46,26 +47,25 @@ class BoolField:
 
 class IntegerField:
 
-    def __init__(self, *, byte_number: int, db_number: int) -> None:
+    def __init__(self, *, byte_number: int) -> None:
         self._byte_size = INTEGER_BYTE_SIZE
         self._byte_number = byte_number
-        self._db_number = db_number
 
         # Keeping track of each instance
         self._values = WeakKeyDictionary()
 
-    def __get__(self, instance, instance_type):
+    def __get__(self, instance, instance_type) -> None:
+        #! Note: this variable is used to get the db_number from the instance
+        #! is very important since with db_number we can get the data from the PLC
+        db_number = instance_type.db_number
         if instance is None:
             return self
         # TODO: Add logic for data retrieve from byte
         return self._values.get(instance)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance, value) -> None:
+        # db_number = instance.db_number
         if not (0 <= value <= MAX_ALLOWED_INTEGER):
             raise ValueError(f"Value must be between 0 and {MAX_ALLOWED_INTEGER}")
         # TODO: Add logic for sending data to PLC
         self._values[instance] = value
-
-
-class StatusDataBlock:
-    level = IntegerField(byte_number=0, db_number=4)
--- tests/test.py
+++ tests/test.py
@@ -2,13 +2,23 @@ from dendrite.siemens.plc import data_block as siemens
 
 
 class StatusDataBlock(siemens.DataBlock):
+    db_number = 20
 
-    level = siemens.IntegerField(byte_number=0, db_number=4)
-    pump_running = siemens.BoolField(byte_number=2, bit_position=0, db_number=4)
+    level = siemens.IntegerField(byte_number=5)
+    level2 = siemens.IntegerField(byte_number=5)
+    pump_running = siemens.BoolField(byte_number=2, bit_position=0)
+
+
+# class ExampleDataBlock(siemens.DataBlock):
+#     db_number = 4
 
 
 if __name__ == "__main__":
     status = StatusDataBlock()
 
     status.level
-    status.level = 30
+    status.level2
+    # status.level = 20
+    status.pump_running
+    # print(status.level._by_size)
+    # status.level = 30
